91219607e81866fb810a41936415baba6f3e6c04
CLN <89996557+CNuenthel@users.noreply.github.com>
2022-04-15 15:29:32 -0500

    Lots

--- NuenthelHub/TKChores/chores/chorespath.py
+++ NuenthelHub/TKChores/chores/chorespath.py
@@ -1,8 +1,7 @@
-""" Holds direct paths for either PC """
+""" Shortcut variable for chores.json path, must be set to chores file location on movement of the folder """
 
-
-# Laptop
-
+# Code Laptop
+chores_path = "C:/Users/cnuen/OneDrive/Desktop/Github/HomeHub/NuenthelHub/TKChores/chores/chorespath.py"
 
 # Main PC
-chores_path = "Z:/OneDrive/Git/HomeHub/NuenthelHub/TKChores/chores/chores.json"
+# chores_path = "Z:/OneDrive/Git/HomeHub/NuenthelHub/TKChores/chores/chores.json"
